cd550bd9df8cc6b47265f4b490f323d095b7dedc
Jay <jay.rabjohns@gmail.com>
2020-10-26 01:39:03 +0000

    Added IP & Port input fields

--- Chat Program/Chat Program/Chat Program/MainWindow.xaml.cs
+++ Chat Program/Chat Program/Chat Program/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +20,53 @@ namespace Chat_Program
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class MainWindow : Window, INotifyPropertyChanged
 	{
+		private IPAddress _ipAddress;
+		public IPAddress IPAddress 
+		{
+			get => _ipAddress;
+			set
+			{
+				if (!_ipAddress.Equals(value))
+				{
+					_ipAddress = value;
+					RaisePropertyChanged("IPAddress");
+				}
+			}
+		}
+
+		private int _port;
+		public int Port
+		{
+			get => _port;
+			set
+			{
+				if (_port != value)
+				{
+					_port = value;
+					RaisePropertyChanged("Port");
+				}
+			}
+		}
+
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		#region INotifyPropertyChanged
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void RaisePropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+		#endregion
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+
 		}
 	}
 }
