e94538bfe57cc9b4e82e87a8cac83830f1152967
Jay <jay.rabjohns@gmail.com>
2021-01-09 11:40:39 +0000

    Copied old server code over to new project

---
+++ Chat Server/Chat Server/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace Chat_Server
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// e2ee with group chats
+			// https://jameshfisher.com/2017/10/25/end-to-end-encryption-with-server-side-fanout/
+
+			// probably use openSSL
+
+			Server server = new Server();
+
+			Thread newConnectionsThread = new Thread(port => server.ListenForConnections((int)port));
+			newConnectionsThread.Start(5000);
+		}
+	}
+}
---
+++ Chat Server/Chat Server/Server.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Chat_Server
+{
+	/// <summary>
+	/// Deals with receiving / sending responses for clients
+	/// </summary>
+	class Server
+	{
+		public void ListenForConnections(int port)
+		{
+			TcpListener tcpListener = new TcpListener(IPAddress.Any, port);
+			tcpListener.Start();
+
+			Console.WriteLine($"Listening for new connections...{Environment.NewLine}");
+			try
+			{
+				while (true)
+				{
+					// Blocks thread until new connection found
+					TcpClient newConnection = tcpListener.AcceptTcpClient();
+					Console.WriteLine("Someone is connecting...");
+
+					
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.ToString());
+			}
+		}
+	}
+}
