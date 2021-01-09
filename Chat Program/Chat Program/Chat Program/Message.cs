9ba0e534ae9ffac033064e3896ad7875323fb553
Jay <jay.rabjohns@gmail.com>
2021-01-09 11:42:47 +0000

    Created new Message class

---
+++ Chat Program/Chat Program/Chat Program/Message.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace Chat_Program
+{
+	public class Message : INotifyPropertyChanged
+	{
+		private string _contents = string.Empty;
+		public string Contents 
+		{
+			get => _contents;
+			set
+			{
+				if (value != _contents)
+				{
+					_contents = value;
+					OnPropertyChanged("Contents");
+				}
+			}
+		}
+
+		#region INotifyPropertyChanged
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+		#endregion
+	}
+}
