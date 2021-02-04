af9677ea79d2a40514b36536c5098d79edbc0ff1
Jay <jay.rabjohns@gmail.com>
2021-02-04 14:56:10 +0000

    Making sure most recent message is always in view

--- Chat Program/Chat Program/Chat Program/Custom Controls/Conversation.xaml.cs
+++ Chat Program/Chat Program/Chat Program/Custom Controls/Conversation.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,11 +23,47 @@ namespace Chat_Program.Custom_Controls
 	public partial class Conversation : UserControl
 	{
 		public ObservableCollection<ConversationMessages> Messages { get { return Globals.ConversationMessages; } }
+		private ListBox ConversationListBox = null;
 
 		public Conversation()
 		{
 			InitializeComponent();
 			DataContext = this;
+		}
+
+		private void MessagesListBox_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+		{
+			if (VisualTreeHelper.GetChildrenCount(ConversationListBox) > 0)
+			{
+				Border border = (Border)VisualTreeHelper.GetChild(ConversationListBox, 0);
+				ScrollViewer scrollViewer = (ScrollViewer)VisualTreeHelper.GetChild(border, 0);
+				scrollViewer.ScrollToBottom();
+			}
+
+			//if (e.Action == NotifyCollectionChangedAction.Add)
+			//{
+			//	ConversationListBox.SelectedItem = e.NewItems[0];
+			//}
+		}
+
+		private void MessagesListBox_Loaded(object sender, RoutedEventArgs e)
+		{
+			if (sender is ListBox listBox 
+				&& listBox.ItemsSource is INotifyCollectionChanged itemsSource)
+			{
+				itemsSource.CollectionChanged += new NotifyCollectionChangedEventHandler(MessagesListBox_CollectionChanged);
+				ConversationListBox = listBox;
+			}
+		}
+
+		private void MessageListBox_Unloaded(object sender, RoutedEventArgs e)
+		{
+			if (sender is ListBox listBox 
+				&& listBox.ItemsSource is INotifyCollectionChanged itemsSource)
+			{
+				itemsSource.CollectionChanged -= new NotifyCollectionChangedEventHandler(MessagesListBox_CollectionChanged);
+				ConversationListBox = null;
+			}
 		}
 	}
 }
