02d085f7d965efbf2e914d51aea59d3823512e3d
Jay Rabjohns <52382074+jayrabjohns@users.noreply.github.com>
2022-02-05 15:01:23 +0000

    Created class for default settings

---
+++ Chat Server/Chat Server/Data/Settings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Chat_Server.Data
+{
+	class Security
+	{
+		public int Iterations { get; } = 2048;
+		public int KeySize = 256;
+		public HashAlgorithmName HashAlgorithm = HashAlgorithmName.SHA1;
+		public CipherMode CipherMode = CipherMode.CBC;
+	}
+
+	class Settings
+	{
+		public static Security Security { get; }
+	}
+}
