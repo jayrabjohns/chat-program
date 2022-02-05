7f31782c3ade6c5335aae6970193d24d1ec6c9ef
Jay Rabjohns <52382074+jayrabjohns@users.noreply.github.com>
2022-02-05 15:02:20 +0000

    Created class for handeling symmetric encryption

---
+++ Chat Server/Chat Server/Security/EncryptedText.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat_Server.Security
+{
+	class EncryptedText
+	{
+		public byte[] CipherTextBytes;
+		public byte[] VectorBytes;
+		public byte[] SaltBytes;
+
+		private string _cipherTextBase64 = null;
+		public string CipherTextBase64 
+		{
+			get
+			{
+				if (_cipherTextBase64 == null)
+				{
+					_cipherTextBase64 = Convert.ToBase64String(CipherTextBytes);
+				}
+
+				return _cipherTextBase64;
+			}
+		}
+	}
+}
