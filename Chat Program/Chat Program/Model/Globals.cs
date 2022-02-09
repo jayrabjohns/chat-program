382e3bbba17e01825c4fefb9954b0333adc60dae
Jay Rabjohns <52382074+jayrabjohns@users.noreply.github.com>
2022-02-09 18:53:41 +0000

    Added path to project root & settings class

--- Chat Program/Chat Program/Model/Globals.cs
+++ Chat Program/Chat Program/Model/Globals.cs
@@ -9,5 +9,22 @@ namespace Chat_Program.Model
 	{
 		public static IConversation CurrentConversation { get => (Conversations.Count > 0 ? Conversations[0] : new Conversation(null)); }
 		public static List<IConversation> Conversations { get; } = new List<IConversation>();
+
+		private static string _projectRootFolder = null;
+		public static string ProjectRootFolder
+		{
+			get
+			{
+				if (_projectRootFolder == null)
+				{
+					var path = System.Reflection.Assembly.GetEntryAssembly().Location;
+					var directory = System.IO.Path.GetDirectoryName(path);
+					_projectRootFolder = directory.Replace(@"\bin\Debug", string.Empty);
+					_projectRootFolder = _projectRootFolder.Replace(@"\net5.0", string.Empty);
+				}
+
+				return _projectRootFolder;
+			}
+		}
 	}
 }
